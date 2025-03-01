e0bf4983197e122556cba89414b053278330fc3d
MarrDev <mecno227@gmail.com>
2025-02-28 22:55:00 -0500

    Primeras pruebas

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
+
+import icon from './assets/icon.png';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+            <Text 
+            style={{
+              color: 'white', 
+              fontSize: 30, 
+              marginTop: 0}}>
+              Mi aplicacion Metacritic
+            </Text>
+      <Image source={{uri: "https://w7.pngwing.com/pngs/409/654/png-transparent-new-super-mario-bros-2-new-super-mario-bros-2-super-mario-super-mario-bros-hand-nintendo.png"}} style={{width: 200, height: 250, marginTop: 45}}/>
+      
+      <Pressable
+      style={{
+        width: 150,
+        height: 50,
+        backgroundColor: 'white',
+        color: 'black',
+        padding: 10,
+        marginTop: 45,
+        borderRadius: 10,
+        alignItems: 'center',
+      }}> 
+        <Text
+        style={{
+          fontSize: 20,
+        }}
+        >Iniciar sesión</Text>
+      </Pressable>
+        
+
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,7 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
